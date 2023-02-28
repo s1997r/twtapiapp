0a355337568a49637be121c887282fc705937994
Shanu Rinkeshwar <65938566+s1997r@users.noreply.github.com>
2023-02-28 15:57:40 +0530

    Create twt_data_api.py

---
+++ twt_data_api.py
@@ -0,0 +1,31 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, jsonify
+#from flask import request
+import snscrape.modules.twitter as sntwitter
+
+app = Flask(__name__)
+
+
+def get_data(query):
+    data = []
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
+        # Number of posts or tweets you want to scrape
+        # comment out if and break statement if you want to get all the tweets
+        if i >= 50:
+            break
+        print(i, tweet, tweet.date, tweet.user.username, tweet.content)
+        tmp_data = {"index_count":i,"tweet_link":tweet.url,
+                    "username":tweet.user.username,"tweet_text":tweet.content}
+        twitter_data = tmp_data
+        data.append(twitter_data)
+    return data
+
+@app.route('/<string:name>',methods = ['GET','POST'])
+def get_user_details(name:str):
+    twitter_data=get_data(name)
+    return twitter_data
+
+if '__name__'== '__main__':
+    app.run()

b57740a9404be4d1ec79991b4e9c2c1c982162dc
Shanu Rinkeshwar <65938566+s1997r@users.noreply.github.com>
2023-03-26 16:05:03 +0530

    Update trend_api_twt.py

--- trend_api_twt.py
+++ trend_api_twt.py
@@ -15,7 +15,7 @@ def get_data(category, time):
     # Filter data based on input parameters
     filtered_df = df[(df['location_name'] == category)]
     # Convert filtered data to JSON format and return as a response
-    data = jsonify(filtered_df.to_dict(orient='records'))}
+    data = jsonify(filtered_df.to_dict(orient='records'))
     return {"data": data}
 
 if __name__ == "__main__":
